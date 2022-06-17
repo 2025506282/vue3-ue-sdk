5e25ce1bae1e69b38eee37406d9e1b8214593ef9
sunji <2025506282@qq.com>
2022-06-17 16:37:35 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import "./dist/index.css"
+import { UEPlayer } from './dist/bundle.esm.js'
 export * from './dist/bundle.esm.js'
 const install = (app) => {
     app.component('UEPlayer', UEPlayer);
