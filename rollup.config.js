5af77725a9efd430e0c01f86731cf7d9aba41c7a
sunji <2025506282@qq.com>
2022-07-22 13:29:39 +0800

    update vue2

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: sunji 2025506282@qq.com
+ * @Date: 2022-06-17 13:43:13
+ * @LastEditors: sunji 2025506282@qq.com
+ * @LastEditTime: 2022-07-22 11:41:41
+ * @FilePath: \vue-ue-sdk\rollup.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import babel from 'rollup-plugin-babel'
 import resolve from "@rollup/plugin-node-resolve";
 import VuePlugin from 'rollup-plugin-vue'
@@ -22,7 +30,7 @@ export default {
     format: 'esm', //若打包commonjs
     assetFileNames: "[name]-[hash][extname]"
   },
-  external: ["vue", "@vue/compiler-sfc"],
+  // external: ["vue", "@vue/compiler-sfc"],
   plugins: [
 
     resolve(),
@@ -46,7 +54,7 @@ export default {
     // css: false 将<style>块转换为导入语句，rollup-plugin-css-only可以提取.vue文件中的样式       
     unassert(),
     terser()
-    
+
   ],
   treeshake: {
     moduleSideEffects: false,
