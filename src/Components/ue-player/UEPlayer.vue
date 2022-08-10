cf57e62783343c8cc316b42ff9defff3d609fd62
sunji <2025506282@qq.com>
2022-08-10 14:51:10 +0800

    修改视频央视

--- src/Components/ue-player/UEPlayer.vue
+++ src/Components/ue-player/UEPlayer.vue
@@ -2,7 +2,7 @@
  * @Author: sunji 2025506282@qq.com
  * @Date: 2022-07-27 16:08:05
  * @LastEditors: sunji 2025506282@qq.com
- * @LastEditTime: 2022-08-02 14:38:08
+ * @LastEditTime: 2022-08-10 14:50:23
  * @FilePath: \vue-ue-sdk\src\Components\ue-player\UEPlayer.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,7 +12,7 @@
     <video
       id="streamingVideo"
       playsinline
-      style="width: 100%; height: 100%; position: absolute"
+      style="width: 100%; height: 100%; position: absolute; object-fit: fill"
     />
   </div>
 </template>
